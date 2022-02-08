3366e71f25c136de342490a0c8cea7c408e05701
GomolemoNdlovu <98579925+GomolemoNdlovu@users.noreply.github.com>
2022-02-08 12:11:55 +0200

    Create AnimPlay.cs
    ADDED ANIME SCRIPT

---
+++ SharedTemplate/Assets/ProjectName_Scripts/AnimPlay.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimPlay : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Animator wire;
+    public bool AnimOn;
+   
+  
+
+    void Start()
+    {
+       // wire=GetComponent<Animator>();
+         AnimOn=false;
+
+    }
+
+
+    public void wireOn()
+    {
+ /*
+        if(AnimOn)
+        {
+       wire.enabled=false;
+       wire.enabled=true;
+        wire.Play("Demo Room Test");
+        AnimOn=false;
+
+        Debug.Log("unclicked"); 
+        }
+
+         */
+       
+    }
+
+
+    public void BoolControl()
+    {
+        AnimOn=true;
+        Debug.Log("clicked");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(AnimOn){
+        wire.Play("Demo Room Test");
+        AnimOn=false;
+        Debug.Log("Unclicked");
+   }
+
+}
+}
